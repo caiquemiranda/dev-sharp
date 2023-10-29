0e4fae9570c83c379a28940d7a9feaa8eb599e2f
caiquemiranda <caiquemirandas@gmail.com>
2023-10-29 20:17:48 -0300

    first-step

---
+++ Enemy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    int speed;
+    string enemyName;
+
+    void Movement()
+    {
+
+
+    }
+
+    void Attack()
+    {
+
+        
+    }
+
+}
